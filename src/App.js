2aca71dae81d9e230ddf7f580d7dfc569a58e6ab
martinhilary <martinhilary96@gmail.com>
2019-09-14 12:46:18 +0800

    testing out the api json and figuring out the makeup of the data

--- src/App.js
+++ src/App.js
@@ -1,26 +1,31 @@
-import React from 'react';
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
 
+const API = 'http://dummy.restapiexample.com/api/v1/employees'
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      employees: [],
+    };
+  }
+
+  componentDidMount() {
+    fetch(API)
+      .then(response => response.json())
+      .then(data => console.log(data[0]))
+    // this.setState({ employees: data })
+    console.log(this.state.employees);
+  }
+  render() {
+    return (
+      <div className="App" >
+
+      </div>
+    );
+  }
+}
 export default App;
