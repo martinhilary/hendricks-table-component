09b3b16ae43f5a18e87ab2814b3850e57e700946
martinhilary <martinhilary96@gmail.com>
2019-09-15 19:14:56 +0800

    added basic search render function

--- src/App.js
+++ src/App.js
@@ -1,54 +1,30 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
-
-
+import Table from './Table';
 const API = 'http://dummy.restapiexample.com/api/v1/employees'
 class App extends Component {
+
   constructor(props) {
     super(props);
-
-    this.state = {
-      employees: [],
-    };
+    this.state = { data: [] };
   }
 
+  // App contains the fetch method
   componentDidMount() {
     fetch(API)
       .then(response => response.json())
-      .then(data => this.setState({
-        employees: data,
-      })
+      .then(data => this.setState({ data })
+        // Update the App state with the new data
       )
-    // need to do a map ontop with id 
-    //users.map(user => {
-    // const { username, name, email } = user;
-    // this.setState({ employees: data })
-    console.log(this.state.employees);
   }
 
-  renderTableData() {
-    // return this.state.
-    console.log(this.state.employees);
-    return this.state.employees.map((employee) => {
-      const { employee_age, employee_name, employee_salary, id, profile_image } = employee //destructuring
-      return (
-        <tr key={id}>
-          <td>{employee_name}</td>
-          <td>{employee_age}</td>
-        </tr>
-      )
-    })
-  }
   render() {
+    const { data } = this.state;
     return (
       <div className="App" >
-        <h1 id='title'>Hendricks Table Data</h1>
-        <table id='employees'>
-          <tbody class='table'>
-            {this.renderTableData()}
-          </tbody>
-        </table>
+        <Table data={data} />
       </div>
     );
   }
--- src/Table.js
+++ src/Table.js
@@ -1,2 +1,79 @@
-import React from 'react'
+import React, { Component } from 'react';
 import style from './table.module.css'
+
+const API = 'http://dummy.restapiexample.com/api/v1/employees'
+class Table extends Component {
+    constructor(props) {
+        super(props);
+        this.renderTableData = this.renderTableData.bind(this);
+        this.state = {
+            search: '',
+            employee: props.employee,
+        };
+    }
+
+    renderTableData(employees) {
+        // return this.state.
+        return employees.map((employee) => {
+            //append two other column data
+            //provide a true/false for each of the elements
+            //map another header component from my side to create a full table with checklist and checkbox using true and false
+
+            const { employee_age, employee_name, employee_salary, id, profile_image } = employee //destructuring
+            return (
+                <tr key={id}>
+                    <td>{id}</td>
+                    <td>{employee_name}</td>
+                    <td>{employee_age}</td>
+                    <td>{employee_salary}</td>
+                    <td>{profile_image}</td>
+                </tr>
+            )
+        })
+    }
+    renderTableHeader() {
+        let obj = { id: "170887", employee_name: "Sourabh006", employee_salary: "50000", employee_age: "26", profile_image: "" }
+        //Not sure why Object keys does not work with my json obj when
+        // I did the Object.keys with (this.props.data[0])
+        let header = Object.keys(obj)
+        return header.map((key, index) => {
+            return <th key={index}>{key.toUpperCase()}</th>
+        })
+    }
+
+    updateSearch(event) {
+        this.setState({ search: event.target.value.substr(0, 20) });
+    }
+
+    render() {
+        let filteredName = this.props.data.filter(
+            //Everytime search equals to name then we return
+            (employee) => {
+                //if cannot find this.state.search as an index of employee obj then do not return it
+                return employee.employee_name.indexOf(this.state.search) !== -1;
+            }
+        );
+        console.log("Filtered Name");
+        console.log(filteredName);
+        return (
+            <div className="Table" >
+                <h1 id='title'>Hendricks Table Data</h1>
+                <input type="text"
+                    placeholder="Search"
+                    value={this.state.search}
+                    onChange={this.updateSearch.bind(this)}
+                />
+                {/* <form>
+                    <>
+                </form> */}
+                <table id='employees'>
+                    <tbody className='table'>
+                        <tr>{this.renderTableHeader()}</tr>
+                        {this.renderTableData(filteredName)}
+                    </tbody>
+                </table>
+            </div>
+        );
+    }
+}
+export default Table;
